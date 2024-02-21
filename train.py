e9658e0d3ed590884c0ae4c9b686904393e5a91c
kossiagbeve <kapab07@gmail.com>
2024-02-21 10:59:38 -0500

    Modified Train.py with a new fxn

--- train.py
+++ train.py
@@ -1,4 +1,9 @@
 def say_hello(name):
     print("Hello DVC & Git for Data Science".format(name))
 
+def greetings(name):
+    print("This is a greeting from {}".format(name))
+
 say_hello("John")
+greetings('John')
+
